e66ca7227154a6ccb586574779a889b325774a09
CVCarter <christinevirginiacarter@gmail.com>
2024-08-29 11:59:49 -0700

    Initial commit

---
+++ lab1.py
@@ -0,0 +1,29 @@
+"""for instructions see: https://github.com/THartmanOfTheRedwoods/PyLab001"""
+
+import math
+
+area = (math.pi * (5**2))
+print(area)
+
+volume = ((4 / 3) * math.pi * 3**3)
+print(volume)
+
+hypotenuse = math.sqrt(3**2 + 4**2)
+print(hypotenuse)
+
+myname = 'Christine Virginia Carter'
+print(len(myname))
+
+fullname = 'Christine' + 'Carter'
+print(fullname.lower)
+print(fullname.upper)
+
+age = 40
+htinft = 5 + (7 / 12)
+htinin = 67
+wtinpds = 205
+print(type(age))
+print(type(htinft))
+print(type(wtinpds))
+BMI = (wtinpds / htinin**2) * 703
+print(BMI)
